539cc2e3a1ddbbb3ab3bb6c2457375a2c3cf5069
Angel Serrato <serratoariasangel@gmail.com>
2022-06-02 22:05:48 -0500

    Add style to it

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ const API_KEY = "6e1db7f6143f00121fa0b58865f90408";
 
 const fetchData = position => {
     const { latitude, longitude } = position.coords;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
-        .then(response => console.log(response))
+    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => setWeatherData(data))
+}
+
+const setWeatherData = data => {
+    console.log(data);
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date: getDate(),
+    }
+
+    Object.keys(weatherData).forEach(key => {
+        document.getElementById(key).textContent = weatherData[key];
+    });
+}
+
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${(`0` + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
 }
 
 const onLoad = () => {
